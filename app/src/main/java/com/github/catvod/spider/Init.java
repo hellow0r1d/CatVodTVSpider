e26a0b02b6923e0665c8e0e19599fa60457af2a6
helloworld <34340469+hellow0r1d@users.noreply.github.com>
2024-01-01 22:59:45 +0800

    update

--- app/src/main/java/com/github/catvod/spider/Init.java
+++ app/src/main/java/com/github/catvod/spider/Init.java
@@ -1,11 +1,13 @@
 package com.github.catvod.spider;
 
 import android.content.Context;
+import android.widget.Toast;
 
 import com.github.catvod.crawler.SpiderDebug;
 
 public class Init {
     public static void init(Context context) {
         SpiderDebug.log("自定义爬虫代码加载成功666！");
+        Toast.makeText(context, "1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9", Toast.LENGTH_SHORT).show();
     }
 }
