4c8212af2f5879de00f38c57b0a8bf11e0595bc5
helloworld <34340469+hellow0r1d@users.noreply.github.com>
2024-01-01 17:56:47 +0800

    update

--- app/src/main/java/com/github/catvod/spider/Init.java
+++ app/src/main/java/com/github/catvod/spider/Init.java
@@ -6,6 +6,6 @@ import com.github.catvod.crawler.SpiderDebug;
 
 public class Init {
     public static void init(Context context) {
-        SpiderDebug.log("自定义爬虫代码加载成功！");
+        SpiderDebug.log("自定义爬虫代码加载成功666！");
     }
 }
