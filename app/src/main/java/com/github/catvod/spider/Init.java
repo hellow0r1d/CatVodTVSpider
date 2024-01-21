45e6f936a012cf5b823f780896a66ab59dab6769
helloworld <34340469+hellow0r1d@users.noreply.github.com>
2024-01-21 15:53:12 +0800

    update version message

--- app/src/main/java/com/github/catvod/spider/Init.java
+++ app/src/main/java/com/github/catvod/spider/Init.java
@@ -8,6 +8,6 @@ import com.github.catvod.crawler.SpiderDebug;
 public class Init {
     public static void init(Context context) {
         SpiderDebug.log("自定义爬虫代码加载成功666！");
-        Toast.makeText(context, "1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9", Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, "2024-01-21", Toast.LENGTH_SHORT).show();
     }
 }
